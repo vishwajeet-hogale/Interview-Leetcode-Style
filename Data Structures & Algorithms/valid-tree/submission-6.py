61bef8e5d9abd57839468d375ff0d0f4c882775c
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2026-06-14 13:24:20 -0700

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from typing import List
+
+
+class UF:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, a):
+        if self.parent[a] == a:
+            return a
+        return self.find(self.parent[a])
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        uf = UF(n=n)
+        for s, d in edges:
+            leader_a = uf.find(s)
+            leader_b = uf.find(d)
+
+            if leader_a == leader_b:
+                return False
+            uf.parent[leader_b] = leader_a
+            
+        print(uf.parent)
+        return True
