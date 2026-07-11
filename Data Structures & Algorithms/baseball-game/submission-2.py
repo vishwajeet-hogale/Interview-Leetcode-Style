a88be3f39453f003c13d6ae26bd8af29cea3702b
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2026-07-10 17:50:59 -0700

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = deque()
+        s = 0
+        for op in operations:
+            if op not in ["+", "C", "D"]:
+                stack.append(int(op))
+                s += int(op)
+
+            elif op == "D" and stack:
+                num1 = int(stack[-1])
+                stack.append(num1*2)
+                s += num1*2
+            elif op == "+" and len(stack) >= 2:
+                num1 = int(stack[-1])
+                num2 = int(stack[-2])
+                s += num1 + num2
+                stack.append(num1 + num2)
+
+            else:
+                if stack:
+                    num1 = int(stack.pop())
+                    print("C: ", num1)
+                    s -= num1
+
+            print(s)
+
+        return s
+                
+
